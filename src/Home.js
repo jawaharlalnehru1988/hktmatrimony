097e5b3c78afeda39f9ebb06006af3b9b5c0fb80
nehru00123@gmail.com <nehru00123@gmail.com>
2024-06-15 13:06:11 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import './Home.css';
+
+function Home() {
+    return (
+      <div className="header-class">
+      <div className='container'>
+        <div className='row'>
+
+      <div className='col-lg-4'>Article</div>
+      <div className='col-lg-4'>Login</div>
+      <div className='col-lg-4'>Contact</div>
+        </div>
+      
+      </div>
+      </div>
+      
+        )
+    }
+
+    export default Home;
