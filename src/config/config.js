63a3051dbd940dfb0c98776e84e020f1ed0481a1
nehru00123@gmail.com <nehru00123@gmail.com>
2024-08-01 08:02:51 +0530

    Architecture are changed

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,10 @@
 const SIDE_MENU = [
-  {key:"videos", label: "Videos", visible: true},
+  {key:"registration", label: "Registration", visible: true},
+  {key:"findMatch", label: "Find Best Matches", visible: true},
+  {key:"contacts", label: "Contacts", visible: true},
   {key:"questions-answers", label: "Questions/Answers", visible: true},
   {key:"articles", label: "Articles", visible: true},
-  {key:"registration", label: "Registration", visible: true},
-  {key:"matrimony", label: "Matrimony", visible: true}
+  {key:"astrology", label: "Astrological Match", visible: true},
 ]
 
 export {SIDE_MENU}
